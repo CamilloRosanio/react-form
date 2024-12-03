42b784d06b403b7f12bff556983d8dacef960559
MyUsername <rosanio.c@gmail.com>
2024-12-03 14:01:00 +0100

    creazione struttura STATICA

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,12 +11,28 @@ function Header() {
                 <div className="container">
 
                     <form action="" className='debug'>
-                        <input className='debug' type="text" />
-                        <button className='debug'>Ricerca</button>
+                        <input className='debug' type="text" placeholder='ID' />
+                        <input className='debug' type="text" placeholder='Author' />
+                        <input className='debug' type="text" placeholder='Title' />
+                        <input className='debug' type="text" placeholder='State' />
+
+                        <button className='debug'>Search</button>
                     </form>
 
-
-
+                    <ul className="feed">
+                        {Articles.map(article => (
+                            <li key={article.id} className=''>
+                                <div className='feedItem'>
+                                    <h4>ID</h4>
+                                    <button>Modify Title</button>
+                                    <button>Delete</button>
+                                    <h3>Title</h3>
+                                    <p>Content</p>
+                                    <p>Author</p>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
 
                 </div>
             </main>
